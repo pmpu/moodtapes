8f8c673c34715f644f1620117ceb6443522b5796
George <spamgoga@gmail.com>
2015-04-25 04:23:53 +0300

    rand str method

--- classes/Utils.php
+++ classes/Utils.php
@@ -6,6 +6,16 @@ class Utils{
     public static function  checkEmail($str){
         return filter_var($str, FILTER_VALIDATE_EMAIL);
     }    
+    
+    function randString($length = 16) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
 }
 
 
