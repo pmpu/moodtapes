5bc3118008f7a5e170fb04186237d292358778e4
George <spamgoga@gmail.com>
2015-04-25 14:22:04 +0300

    image, music controllerooasd

--- classes/ApiController.php
+++ classes/ApiController.php
@@ -13,11 +13,9 @@ class ApiController{
 	   $cusr = UserController::currentUser();
 	   switch($this->request->getAction()){
 	       case "":
-              echo "main page";
+              Header("Location: /all");
            break;
-	       case "hello":
-            echo "hello ".$this->request->getParam(1); 
-           break;
+	       
            case "signup":
             $uc = new UserController();
             $uc->signup($this->request->getPost("email"), $this->request->getPost("password"), $this->request->getPost("name"));
@@ -64,11 +62,7 @@ class ApiController{
            case "get_uploaded_music":
                 MusicController::getUserMusic($cusr);
            break;
-           case "dbcheck":            
            
-           print_r(Db::getElementByQuery("SELECT * FROM users"));
-             print_r($_POST);
-           break;
            default:
              echo "no such action";
 	   }
--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 require_once "config.php";
 	$controller = new ApiController(new Request($_GET['req'], $_POST, $_FILES));
 	$controller->processRequest();
